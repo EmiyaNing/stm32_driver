0ea79b4420439239506dd12bf911c76f684b3789
EmiyaNing <ningkangl@icloud.com>
2019-07-02 21:55:49 +0800

    change main.c 2019 7.2 21:55

--- USER/main.c
+++ USER/main.c
@@ -1,88 +1,143 @@
 /* LED 
 
-核心板LED PA5,
+?????LED PA5,
 
 */
 
+
 #include "stm32f10x.h"
+#define KEY_ON 1
+
  void Delay(u32 count)
  {
   u32 i=0;
   for(;i<count;i++);
  }
-/**
- * 我就先设置3个LED两灭模式:
- * 红色D3常亮 0
- * 绿色D4常亮 1
- * 红绿交替亮 2
- */
- void light_LED(int select){
-   switch(select){
-     case 0:{
-       GPIO_SetBits(GPIOB, GPIO_Pin_6);
-       break;
-     }
-     case 1:{
-       GPIO_SetBits(GPIOA, GPIO_Pin_6);
-       break;
-     }
-     case 2:{
-       while(1){
-         GPIO_ResetBits(GPIOA, GPIO_Pin_6);
-         GPIO_SetBits(GPIOB, GPIO_Pin_6);
-         Delay(3000000);
-         GPIO_ResetBits(GPIOB, GPIO_Pin6);
-         GPIO_SetBits(GPIOA, GPIO_Pin_6);
-         Delay(3000000);
-       }
-     }
-   }
 
+ void light_PWM(){
+	 GPIO_SetBits(GPIOC, GPIO_Pin_3);
+ }
+ 
+ void stop_PWM(){
+	 GPIO_ResetBits(GPIOC, GPIO_Pin_3);
+ }
+ 
+ void right_head(){
+	  GPIO_SetBits(GPIOA,GPIO_Pin_4);
+	  GPIO_ResetBits(GPIOB,GPIO_Pin_9);
+ }
+ 
+ void left_head(){
+	 GPIO_SetBits(GPIOB,GPIO_Pin_7);
+   GPIO_ResetBits(GPIOB,GPIO_Pin_8);	
+ }
+ 
+ void right_back(){
+	  GPIO_SetBits(GPIOA,GPIO_Pin_9);
+	  GPIO_ResetBits(GPIOB,GPIO_Pin_4);
+ }
+ 
+ void left_back(){
+	 GPIO_SetBits(GPIOB,GPIO_Pin_8);
+   GPIO_ResetBits(GPIOB,GPIO_Pin_7);	
+ }
+ 
+ void stop_driver(){
+	 GPIO_ResetBits(GPIOA,GPIO_Pin_4);
+	 GPIO_ResetBits(GPIOB,GPIO_Pin_9);
+	 GPIO_ResetBits(GPIOB,GPIO_Pin_8);
+   GPIO_ResetBits(GPIOB,GPIO_Pin_7);
+ }
+ 
+ void control_driver(int select){
+	 switch(select){
+		 case 0:{
+			 right_head();
+			 left_head();
+			 break;
+		 }
+		 case 1:{
+			 right_back();
+			 left_back();
+			 break;
+		 }
+		 case 2:{
+			 right_head();
+			 left_back();
+			 break;
+		 }
+		 case 3:{
+			 right_back();
+			 left_head();
+			 break;
+		 }
+		 default:{
+			 right_head();
+			 left_head();
+		 }
+	 }	 
  }
  
  int main(void)
  {	
   GPIO_InitTypeDef  GPIO_InitStructure;
  	
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC, ENABLE);	 //使能PA端口时钟
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOB, ENABLE);	 //???PA,B,C??????
 
-// 端口A初始化,
-  /**
-   * 直流电机:
-   * 第一对: PA4, PB9
-   * 第二对: PB7, PB8
-   * LED
-   * 红色D3：PB6
-   * 绿色D4：PA6
-   * 
-   * 蜂鸣器
-   * PC3
-   * 
-   * 主控板LED
-   * PA5
-   */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		  //推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		  //IO口速度为50MHz
-  /**
-   * 讲道理，我觉得这样设置没有问题，虽然可能并不是每一组GPIO口都需要用到上面列举出来的所有io口，但是我觉得这样好像也没有问题。
-   */
-  GPIO_Init(GPIOA, &GPIO_InitStructure);					      //根据设定参数初始化GPIOA.5
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-  /**
-   * 我先直接让蜂鸣器响
-   *  */ 
-  GPIO_SetBits(GPIOC, GPIO_Pin_3);
-
+// ???A?????,	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_4 | GPIO_Pin_9 | GPIO_Pin_7 | GPIO_Pin_8;//PA.5 - ?????LED;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		  //???????
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		  //IO??????50MHz
+  GPIO_Init(GPIOA, &GPIO_InitStructure);					      //?????趨?????????GPIOA.5
+	 
+	//GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 ;//PA.6 - ?????LED;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);					      //?????趨?????????GPIOA.5
+	 
+	 
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 ;//PC.3 - ?????LED;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);					      //?????趨?????????GPIOA.5
+	 /**
+	 * set btn input
+	 */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOC, &GPIO_InitStructure); 
+	 /**
+	 * set find way input
+	 **/
+	 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	 GPIO_Init(GPIOB, &GPIO_InitStructure);
+	 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	 GPIO_Init(GPIOA, &GPIO_InitStructure);
+	 
   while(1)
 	{
-	  GPIO_ResetBits(GPIOA,GPIO_Pin_5);
-		//Delay(3000000);
-		light_LED(2);
-		GPIO_SetBits(GPIOA,GPIO_Pin_5);
-		//Delay(3000000);	
+		if(GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == 0){
+			GPIO_SetBits(GPIOB,GPIO_Pin_7);
+			GPIO_ResetBits(GPIOB,GPIO_Pin_8);	
+			GPIO_SetBits(GPIOA,GPIO_Pin_6);
+			GPIO_ResetBits(GPIOB,GPIO_Pin_6);
+		}else{
+			GPIO_ResetBits(GPIOB,GPIO_Pin_7);
+			GPIO_ResetBits(GPIOB,GPIO_Pin_8);	
+			GPIO_ResetBits(GPIOA, GPIO_Pin_6);
+			GPIO_ResetBits(GPIOB, GPIO_Pin_6);
+			Delay(300000);
+		}
+		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0){
+			GPIO_SetBits(GPIOA,GPIO_Pin_4);
+			GPIO_ResetBits(GPIOB,GPIO_Pin_9);
+			GPIO_SetBits(GPIOB,GPIO_Pin_6);
+			//GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+		}else{
+			GPIO_ResetBits(GPIOA,GPIO_Pin_4);
+			GPIO_ResetBits(GPIOB,GPIO_Pin_9);
+			GPIO_ResetBits(GPIOA, GPIO_Pin_6);
+			GPIO_ResetBits(GPIOB, GPIO_Pin_6);
+			Delay(300000);
+		}
 	}
 }
